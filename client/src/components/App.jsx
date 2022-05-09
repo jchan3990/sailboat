b5e2a69c80c21d28e8acab7530c0ea234c78710f
Jonathan Chan <jonathan@striim.com>
2022-05-09 15:23:58 -0700

    Added products and styling to page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import data from '../data.js';
 
 const App = () => {
   return (
@@ -6,7 +7,25 @@ const App = () => {
       <header className="App-header">
         <a href="/">Sailboat</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map(product=> (
+            <div className='product' key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>${product.description}</strong></p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
