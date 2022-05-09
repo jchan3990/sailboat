96c92072576ed91a2f9f14d399903281dae3e3f8
Jonathan Chan <jonathan@striim.com>
2022-05-08 22:34:57 -0700

    Create React App

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Sailboat</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
