9bd0b33b51f27a3f21ef44390b37312b62e07e10
Jonathan Chan <jonchan3990@gmail.com>
2022-05-09 23:33:44 -0700

    Bootstrap navbar and routing

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Navbar, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 import Home from './Home.jsx';
 import Product from './Product.jsx';
@@ -7,16 +9,27 @@ import Product from './Product.jsx';
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <Link to="/">Sailboat</Link>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>sailboat</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product/:slug" element={<Product />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/product/:slug" element={<Product />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All Rights Reservered</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
