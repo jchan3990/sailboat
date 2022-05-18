d16dd7a37660bf178a628959833d8d948456a1d1
Jonathan Chan <jonchan3990@gmail.com>
2022-05-17 22:58:23 -0700

    Changed Store to recognize same items being added through item id

--- client/src/store.js
+++ client/src/store.js
@@ -11,7 +11,14 @@ const initialState = {
 const reducer = (state, action) => {
   switch(action.type) {
     case 'CART_ADD_ITEM':
-      return {...state, cart: {...state.cart, cartItems: [...state.cart.cartItems, action.payloud]}};
+      const newItem = action.payload;
+      const itemExists = state.cart.cartItems.find(item => item._id === newItem._id);
+      const cartItems = itemExists ?
+        state.cart.cartItems.map(item => item._id === itemExists._id ?
+          newItem : item)
+        : [...state.cart.cartItems, newItem];
+      return { ...state, cart: { ...state.cart, cartItems }};
+      
     default:
       return state;
   }
