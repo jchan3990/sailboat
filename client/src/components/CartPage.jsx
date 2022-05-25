4eda8eeb969066380aa6a010c60e6aa7c9fb92c3
Jonathan Chan <jonchan3990@gmail.com>
2022-05-25 00:30:34 -0700

    Added functionality to all buttons

--- client/src/components/CartPage.jsx
+++ client/src/components/CartPage.jsx
@@ -1,14 +1,39 @@
 import { React, useContext } from 'react';
 import { Row, Col, ListGroup, Button, Card } from 'react-bootstrap';
 import { Helmet } from 'react-helmet-async'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Store } from '../store.js';
 
 import MessageBox from './MessageBox.jsx';
 
 const CartPage = () => {
+  const navigate = useNavigate();
+  
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { cart: { cartItems } } = state;
+
+  const updateCart = async (item, quantity) => {
+    const response = await fetch(`/api/products/${item._id}`);
+    const data = await response.json();
+
+    if (data.countInStock < quantity) {
+      window.alert('Sorry, product is out of stock');
+      return;
+    }
+
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: {...item, quantity: quantity}
+    });
+  }
+
+  const removeItem = item => {
+    ctxDispatch({ type: 'CART_REMOVE_ITEM', payload: item });
+  }
+
+  const handleCheckout = () => {
+    navigate('/signin?redirect=/shipping');
+  }
 
   return (
     <div>
@@ -32,17 +57,28 @@ const CartPage = () => {
                       <Link to={`/product/${item.slug}`}>{item.name}</Link>
                     </Col>
                     <Col md={3}>
-                      <Button variant="light" disabled={item.quantity === 1}>
+                      <Button
+                        variant="light"
+                        disabled={item.quantity === 1}
+                        onClick={() => updateCart(item, item.quantity - 1)}
+                        >
                         <i className="fas fa-minus-circle"></i>
                       </Button> {' '}
                       <span>{item.quantity}</span>{' '}
-                      <Button variant="light" disabled={item.quantity === 1}>
+                      <Button
+                        variant="light"
+                        disabled={item.quantity === item.countInStock}
+                        onClick={() => updateCart(item, item.quantity + 1)}
+                      >
                         <i className="fas fa-plus-circle"></i>
                       </Button>
                     </Col>
                     <Col md={3}>${item.price}</Col>
                     <Col md={2}>
-                      <Button variant="light">
+                      <Button 
+                        variant="light"
+                        onClick={() => removeItem(item)}
+                      >
                         <i className="fas fa-trash"></i>
                       </Button>
                     </Col>
@@ -58,14 +94,18 @@ const CartPage = () => {
               <ListGroup variant="flush">
                 <ListGroup.Item>
                   <h3>
-                    Subtotal ({cartItems.reduce((a, c) => a+ c.quantity, 0)}{' '}
-                    items) : $
-                    {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
+                    Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}{' '}
+                    items) : ${cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
                   </h3>
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <div className="d-grid">
-                    <Button type="button" variant="primary" disabled={cartItems.length === 0}>
+                    <Button
+                      type="button"
+                      variant="primary"
+                      disabled={cartItems.length === 0}
+                      onClick={handleCheckout}
+                    >
                       Proceed to Checkout
                     </Button>
                   </div>
