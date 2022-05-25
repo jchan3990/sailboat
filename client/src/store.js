345137d2ae51b407a90be1b4bbe9aa558094feed
Jonathan Chan <jonchan3990@gmail.com>
2022-05-25 00:31:52 -0700

    Added remove from cart action

--- client/src/store.js
+++ client/src/store.js
@@ -4,7 +4,7 @@ export const Store = createContext();
 
 const initialState = {
   cart: {
-    cartItems: [],
+    cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [],
   }
 };
 
@@ -17,8 +17,13 @@ const reducer = (state, action) => {
         state.cart.cartItems.map(item => item._id === itemExists._id ?
           newItem : item)
         : [...state.cart.cartItems, newItem];
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems }};
-      
+      case 'CART_REMOVE_ITEM': {
+        const cartItems = state.cart.cartItems.filter(item => item._id !== action.payload._id);
+        localStorage.setItem('cartItems', JSON.stringify(cartItems))
+        return { ...state, cart: { ...state.cart, cartItems } }
+      }
     default:
       return state;
   }
