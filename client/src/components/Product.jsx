83590314fcd65fe938a840542a4691fedf4c9eea
Jonathan Chan <jonchan3990@gmail.com>
2022-05-25 00:31:17 -0700

    Added functionality to "Add to Cart" button

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,10 +1,30 @@
-import { React } from 'react';
+import { React, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Card, Button } from 'react-bootstrap';
+import { Store } from '../store.js';
 
 import Rating from './Rating.jsx';
 
 const Product = ({ product }) => {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart: { cartItems } } = state;
+
+  const addToCart = async (item) => {
+    const itemExists = cartItems.find(x => x._id === product._id);
+    const quantity = itemExists ? itemExists.quantity + 1 : 1;
+    const response = await fetch(`/api/products/${item._id}`);
+    const data = await response.json();
+
+    if (data.countInStock < quantity) {
+      window.alert('Sorry, product is out of stock');
+      return;
+    }
+
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: {...item, quantity: quantity}
+    });
+  }
 
   return (
     <Card>
@@ -17,7 +37,11 @@ const Product = ({ product }) => {
         </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>${product.price}</Card.Text>
-        <Button>Add to Cart</Button>
+        {product.countInStock === 0 ?
+          <Button variant='light' disabled>Out of Stock</Button>
+          :
+          <Button onClick={() => addToCart(product)}>Add to Cart</Button>
+        }
       </Card.Body>
     </Card>
   )
