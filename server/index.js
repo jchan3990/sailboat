28c360fa6a786d84c3312c8e22cb2e68c1839a6d
Jonathan Chan <jonchan3990@gmail.com>
2022-05-26 00:17:55 -0700

    Added express middleware and userRouter path

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -16,12 +16,18 @@ mongoose.connect(process.env.MONGODB_URI)
   });
 
 const app = express();
-
-app.use('/api/seed', seedRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 });
 
+app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
