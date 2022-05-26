5a7db1b6d8926c53171bdf35776c2016cce473e6
Jonathan Chan <jonchan3990@gmail.com>
2022-05-26 16:22:48 -0700

    Added Signin function to signin page

--- client/src/components/SignInPage.jsx
+++ client/src/components/SignInPage.jsx
@@ -1,12 +1,49 @@
-import { React } from 'react';
+import { React, useState, useContext, useEffect } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { Helmet } from 'react-helmet-async';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Store } from '../store.js';
 
 const SignInPage = () => {
+  const navigate = useNavigate();
   const { search } = useLocation();
   const redirectInURL = new URLSearchParams(search).get('redirect');
   const redirect = redirectInURL ? redirectInURL : '/';
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let payload = { email, password };
+
+    const response = await fetch('/api/users/signin', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+    const data = await response.json();
+
+    if (response.status >= 400 && response.status < 600) {
+      alert('Invalid email or password');
+    } else {
+      ctxDispatch({ type: 'USER_SIGNIN', payload: data });
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      navigate(redirect || '/');
+    }
+  }
 
   return (
     <Container className="small-container">
@@ -14,14 +51,14 @@ const SignInPage = () => {
         <title>Sign In</title>
       </Helmet>
       <h1 className="my-3">Sign In</h1>
-      <Form>
+      <Form onSubmit={handleSubmit} >
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" required />
+          <Form.Control type="email" required onChange={(e) => setEmail(e.target.value)}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" required />
+          <Form.Control type="password" required onChange={(e) => setPassword(e.target.value)}/>
         </Form.Group>
         <div className="mb-3">
           <Button type="submit">Sign In</Button>
