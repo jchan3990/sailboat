15646254c03e603e1de0111b9f2e83d81d14c6c9
Jonathan Chan <jonchan3990@gmail.com>
2022-05-31 21:10:04 -0700

    Refactored CheckoutSteps component

--- client/src/components/CheckoutSteps.jsx
+++ client/src/components/CheckoutSteps.jsx
@@ -1,13 +1,13 @@
 import { React } from 'react';
 import { Row, Col } from 'react-bootstrap';
 
-const CheckoutSteps = (props) => {
+const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
   return (
     <Row className="checkout-steps">
-      <Col className={ props.step1 ? 'active' : ''}>Sign-In</Col>
-      <Col className={ props.step2 ? 'active' : ''}>Shipping</Col>
-      <Col className={ props.step3 ? 'active' : ''}>Payment</Col>
-      <Col className={ props.step4 ? 'active' : ''}>Place Order</Col>
+      <Col className={ step1 ? 'active' : ''}>Sign-In</Col>
+      <Col className={ step2 ? 'active' : ''}>Shipping</Col>
+      <Col className={ step3 ? 'active' : ''}>Payment</Col>
+      <Col className={ step4 ? 'active' : ''}>Place Order</Col>
     </Row>
   )
 };
