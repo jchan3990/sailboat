3eda0fe6108a95cb4728bfa2848a32fe46e33753
Jonathan Chan <jonchan3990@gmail.com>
2022-05-31 20:51:08 -0700

    Fixed typo

--- client/src/store.js
+++ client/src/store.js
@@ -47,7 +47,7 @@ const reducer = (state, action) => {
 
 export function StoreProvider(props) {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const value = {state, dispatch};
+  const value = { state, dispatch };
   
   return <Store.Provider value={value}>{props.children}</Store.Provider>
 }
