1c96ca421a651fbda30cdf68dc8dd1f91513bdf2
Jonathan Chan <jonchan3990@gmail.com>
2022-05-31 21:07:20 -0700

    Moved pages to pages folder

---
+++ client/src/pages/SignInPage.jsx
@@ -0,0 +1,75 @@
+import { React, useState, useContext, useEffect } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Helmet } from 'react-helmet-async';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Store } from '../store.js';
+
+const SignInPage = () => {
+  const navigate = useNavigate();
+  const { search } = useLocation();
+  const redirectInURL = new URLSearchParams(search).get('redirect');
+  const redirect = redirectInURL ? redirectInURL : '/';
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let payload = { email, password };
+
+    const response = await fetch('/api/users/signin', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    })
+    const data = await response.json();
+
+    if (response.status >= 400 && response.status < 600) {
+      alert('Invalid email or password');
+    } else {
+      ctxDispatch({ type: 'USER_SIGNIN', payload: data });
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      navigate(redirect || '/');
+    }
+  }
+
+  return (
+    <Container className="small-container">
+      <Helmet>
+        <title>Sign In</title>
+      </Helmet>
+      <h1 className="my-3">Sign In</h1>
+      <Form onSubmit={handleSubmit} >
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" required onChange={(e) => setEmail(e.target.value)}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" required onChange={(e) => setPassword(e.target.value)}/>
+        </Form.Group>
+        <div className="mb-3">
+          <Button type="submit">Sign In</Button>
+        </div>
+        <div className="mb-3">
+          New Customer?{' '}
+          <Link to={`/signup?redirect=${redirect}`}>Create Account</Link>
+        </div>
+      </Form>
+    </Container>
+  )
+}
+
+export default SignInPage;
