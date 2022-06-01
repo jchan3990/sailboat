fa0d6a16991af198bb019e7e7b8fea8e25b9e1f1
Jonathan Chan <jonchan3990@gmail.com>
2022-06-01 00:50:34 -0700

    Added place order functionality

--- client/src/pages/PlaceOrderPage.jsx
+++ client/src/pages/PlaceOrderPage.jsx
@@ -1,4 +1,4 @@
-import { React, useContext, useEffect } from 'react';
+import { React, useContext, useEffect, useReducer } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useNavigate } from 'react-router-dom';
 import { Card, Row, Col, ListGroup, Button } from 'react-bootstrap';
@@ -6,28 +6,78 @@ import { Store } from '../Store.js';
 import { Link } from 'react-router-dom';
 
 import CheckoutSteps from '../components/CheckoutSteps.jsx';
+import LoadingBox from '../components/LoadingBox.jsx';
+
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'CREATE_REQUEST':
+      return { ...state, loading: true };
+    case 'CREATE_SUCCESS':
+      return { ...state, loading: false };
+    case 'CREATE_FAIL':
+      return { ...state, loading: false };
+    
+    default:
+      return state;
+  }
+}
 
 const PlaceOrderPage = () => {
   const navigate = useNavigate();
+
+  const [{ loading, error }, dispatch] = useReducer(reducer, {
+    loading: false,
+    error: ''
+  })
+
   const { state, dispatch: ctxDispatch } = useContext(Store);
-  const { cart: {cartItems, shippingAddress, paymentMethod } } = state;
+  const { cart, userInfo } = state;
 
   const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100;
-  const itemsPrice = round2(cartItems.reduce((a, c) => a + c.quantity * c.price, 0));
-  const shippingPrice = itemsPrice > 100 ? round2(0) : round2(10);
-  const taxPrice = round2(0.15 * itemsPrice);
-  const totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+  cart.itemsPrice = round2(cart.cartItems.reduce((a, c) => a + c.quantity * c.price, 0));
+  cart.shippingPrice = cart.itemsPrice > 100 ? round2(0) : round2(10);
+  cart.taxPrice = round2(0.15 * cart.itemsPrice);
+  cart.totalPrice = round2(cart.itemsPrice + cart.shippingPrice + cart.taxPrice);
 
   const handlePlaceOrder = async() => {
+    dispatch({ type: 'CREATE_REQUEST'} );
+    const body = {
+      orderItems: cart.cartItems,
+      shippingAddress: cart.shippingAddress,
+      paymentMethod: cart.paymentMethod,
+      itemsPrice: cart.itemsPrice,
+      shippingPrice: cart.shippingPrice,
+      taxPrice: cart.taxPrice,
+      totalPrice: cart.totalPrice
+    }
+    const response = await fetch('/api/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${userInfo.token}`
+      },
+      body: JSON.stringify(body)
+    })
 
+    if (response.status >= 400 && response.status < 600) {
+      dispatch({ type: 'CREATE_FAIL' })
+      alert('Oh no! Something went wrong.');
+    } else {
+      const data = await response.json();
+      ctxDispatch({ type: 'CART_CLEAR'} );
+      dispatch({ type: 'CREATE_SUCESS'} );
+      localStorage.removeItem('cartItems');
+      navigate(`/order/${data.order._id}`)
+    }
   }
 
   useEffect(() => {
-    if (!paymentMethod) {
+    if (!cart.paymentMethod) {
       navigate('/payment');
     }
     
-  }, [paymentMethod, navigate])
+  }, [cart, navigate])
 
   return (
     <div>
@@ -42,11 +92,11 @@ const PlaceOrderPage = () => {
             <Card.Body>
               <Card.Title>Shipping</Card.Title>
               <Card.Text>
-                <strong>Name:</strong> {shippingAddress.fullName} <br />
-                <strong>Address:</strong> {shippingAddress.address}, {' '}
-                {shippingAddress.city}, {' '}
-                {shippingAddress.postalCode}, {' '}
-                {shippingAddress.country}
+                <strong>Name:</strong> {cart.shippingAddress.fullName} <br />
+                <strong>Address:</strong> {cart.shippingAddress.address}, {' '}
+                {cart.shippingAddress.city}, {' '}
+                {cart.shippingAddress.postalCode}, {' '}
+                {cart.shippingAddress.country}
               </Card.Text>
               <Link to="/shipping">Edit</Link>
             </Card.Body>
@@ -55,7 +105,7 @@ const PlaceOrderPage = () => {
             <Card.Body>
               <Card.Title>Payment Method</Card.Title>
               <Card.Text>
-                <strong>Method:</strong> {paymentMethod}
+                <strong>Method:</strong> {cart.paymentMethod}
               </Card.Text>
               <Link to="/payment">Edit</Link>
             </Card.Body>
@@ -64,7 +114,7 @@ const PlaceOrderPage = () => {
             <Card.Body>
               <Card.Title>Items</Card.Title>
               <ListGroup variant="flush">
-                {cartItems.map(item => (
+                {cart.cartItems.map(item => (
                   <ListGroup.Item key={item._id}>
                     <Row className="align-items-center">
                       <Col md={6}>
@@ -93,25 +143,25 @@ const PlaceOrderPage = () => {
                 <ListGroup.Item>
                   <Row>
                     <Col>Items</Col>
-                    <Col>${itemsPrice.toFixed(2)}</Col>
+                    <Col>${cart.itemsPrice.toFixed(2)}</Col>
                   </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <Row>
                     <Col>Shipping</Col>
-                    <Col>${shippingPrice.toFixed(2)}</Col>
+                    <Col>${cart.shippingPrice.toFixed(2)}</Col>
                   </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <Row>
                     <Col>Tax</Col>
-                    <Col>${taxPrice.toFixed(2)}</Col>
+                    <Col>${cart.taxPrice.toFixed(2)}</Col>
                   </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <Row>
                     <Col><strong>Order Total</strong></Col>
-                    <Col><strong>${totalPrice.toFixed(2)}</strong></Col>
+                    <Col><strong>${cart.totalPrice.toFixed(2)}</strong></Col>
                   </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
@@ -119,11 +169,12 @@ const PlaceOrderPage = () => {
                     <Button
                       type="button"
                       onClick={handlePlaceOrder}
-                      disabled={cartItems.length === 0}
+                      disabled={cart.cartItems.length === 0}
                     >
                       Place Order
                     </Button>
                   </div>
+                  {loading && <LoadingBox />}
                 </ListGroup.Item>
               </ListGroup>
             </Card.Body>
