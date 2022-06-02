297845c15c1012573d25a66f20d639afd78b38b0
Jonathan Chan <jonchan3990@gmail.com>
2022-06-01 21:41:19 -0700

    Added paypal key endpoint

--- server/index.js
+++ server/index.js
@@ -21,6 +21,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/api/keys/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CID || 'sb');
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
