0c6cf07a89158c0ec7d7a105c2149f4d9a1d18f3
Jonathan Chan <jonchan3990@gmail.com>
2022-06-03 01:14:09 -0700

    Added path and dir to express static for deployment

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
@@ -34,6 +35,10 @@ app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/build')))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/client/build/index.html')));
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
