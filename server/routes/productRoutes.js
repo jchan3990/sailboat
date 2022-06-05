63ac12cb1a82d636fbfae9ff67631ac50aaacdb3
Jonathan Chan <jonchan3990@gmail.com>
2022-06-05 01:59:25 -0700

    Added product categories route

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
 const productRouter = express.Router();
@@ -7,6 +8,11 @@ productRouter.get('/', async (req, res) => {
   const products = await Product.find();
   res.send(products);
 })
+
+productRouter.get('/categories', expressAsyncHandler(async (req, res) => {
+  const categories = await Product.find().distinct('category');
+  res.send(categories);
+}));
 
 productRouter.get('/slug/:slug', async (req, res) => {
   const product = await Product.findOne({ slug: req.params.slug });
@@ -17,7 +23,7 @@ productRouter.get('/slug/:slug', async (req, res) => {
   }
 });
 
-productRouter.get('/:id',async (req, res) => {
+productRouter.get('/:id', async (req, res) => {
   const product = await Product.findById(req.params.id);
   if (product) {
     res.send(product);
