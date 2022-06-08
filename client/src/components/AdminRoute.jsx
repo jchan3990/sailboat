0dbb079d43210675235ac0431b847409899f9b30
Jonathan Chan <jonchan3990@gmail.com>
2022-06-08 13:35:27 -0700

    Check for admin prop or navigate

---
+++ client/src/components/AdminRoute.jsx
@@ -0,0 +1,13 @@
+import { React, useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { Store } from '../store.js';
+
+const AdminRoute = ({ children }) => {
+
+  const { state } = useContext(Store)
+  const { userInfo } = state;
+
+  return userInfo && userInfo.isAdmin ? children : <Navigate to="/signin" />
+}
+
+export default AdminRoute;
